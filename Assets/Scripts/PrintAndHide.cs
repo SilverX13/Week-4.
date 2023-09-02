ef26ab55e37a8111a252000e890b1bf0deb6b97e
SilverX13 <1033759529@qq.com>
2023-09-02 22:26:39 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,11 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    private int randomInt;
  
     // Start is called before the first frame update
     void Start()
     {
-        i++;
+        randomInt = Random.Range(150, 251);
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -18,5 +20,14 @@ public class PrintAndHide : MonoBehaviour
     {
         Debug.Log(gameObject.name + ":" + i);
         i++;
+        if (this.gameObject.tag == "Red" && i == 100) { 
+            this.gameObject.SetActive(false);
+        }
+
+        if (this.gameObject.tag == "Blue" && i == randomInt)
+        {
+            rend.enabled = false;
+
+        }
     }
 }
